2603b66e855430c9cb36044bf21e3eb4d566a203
Rangga Perdana <rdwiperdana@gmail.com>
2024-12-07 16:12:48 +0700

    portofolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#ef4444",
+        dark: "#0f172a",
+        grey: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+}
+
